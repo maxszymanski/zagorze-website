067fccdcb5c50a1ea44813bec1bf1e22117e7596
Max Szymański <maksymilianszymanski2@gmail.com>
2025-05-29 11:50:10 +0200

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import GallerySection from './_components/home/GallerySection'
 import NewsSection from './_components/home/NewsSection'
 import ContactUsSection from './_components/home/ContactUsSection'
 import PeopleSection from './_components/home/PeopleSection'
+import Footer from './_components/home/Footer'
 
 export default function Home() {
 	return (
@@ -18,6 +19,7 @@ export default function Home() {
 				<Council />
 				<ContactUsSection />
 			</main>
+			<Footer />
 		</>
 	)
 }
