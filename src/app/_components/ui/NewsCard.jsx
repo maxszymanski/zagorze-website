424671374e97bc5b830bb57d5d32864ef7c78850
Maksymilian Szymański <maksymilianszymanski2@gmail.com>
2025-06-04 18:08:09 +0200

    merge

--- src/app/_components/ui/NewsCard.jsx
+++ src/app/_components/ui/NewsCard.jsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { formattedDate } from '@/app/utils/helpers'
 import { blurImage } from '@/app/utils/blurImage'
 
 function NewsCard({ large = false, post }) {
